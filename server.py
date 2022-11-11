48d5b5b92860f27ad6b169dbbe1cbbef50c09c4c
kukuruza322 <72981160+kukuruza322@users.noreply.github.com>
2022-11-11 10:03:52 +0300

    add connection with PostgreSQL database server. works great

--- server.py
+++ server.py
@@ -1,24 +1,37 @@
 from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
+import psycopg2
+
+IP = 'localhost'
+PORT = 8000
+
+# connecting to existing db server with table "cities"
+conn = psycopg2.connect(dbname='spa_db', user='postgres', password='Qwerty123', host='localhost', port='5433')
+cursor = conn.cursor()
 
 
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('127.0.0.1', 8000)
+    server_address = (IP, PORT)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
 
 class MyHandler(BaseHTTPRequestHandler):
-    # A custom handler with overridden method do_GET.
+    # A custom request-handler with overridden method do_GET.
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(html.encode())
+        self.send_header("Content-type", "text/html")  # when data is returned, there should be at least one header line of the form Content-type: <type>/<subtype> where <type> and <subtype> should be registered MIME types, e.g. "text/html" or "text/plain".
+        self.end_headers() # blank line for cut-off headers from data
+        # read database
+        # modify view in page.html
+        # print updated view
+        # where should be placed a controller function ?
+        self.wfile.write(html.encode())  # data of response
 
 
 with open('page.html', 'r', encoding='utf-8') as file:
     html = file.read()
 
-run(handler_class=MyHandler)
+if __name__ == '__main__':
+    run(handler_class=MyHandler)
